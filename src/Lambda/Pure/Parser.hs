3d72b02c507add0149f2d636353b9f50eebb2a66
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-07-15 04:59:01 +0200

    Pure lambda term parser work-in-progress

---
+++ src/Lambda/Pure/Parser.hs
@@ -0,0 +1,29 @@
+{-# LANGUAGE MonadComprehensions #-}
+module Lambda.Pure.Parser where
+
+import Text.ParserCombinators.Parsec
+import Lambda.Pure.Term
+
+--parseTerm :: String -> Either ParseError LambdaTerm
+--parseTerm _ = fail "Not implemented"
+
+lambdaTerm :: GenParser Char st LambdaTerm
+lambdaTerm = lambdaVar <|> lambdaApp <|> lambdaLam <|> paren
+
+--lambdaVar :: GenParser Char st LambdaTerm
+lambdaVar = [Var v | v <- variableName]
+
+--lambdaApp :: GenParser Char st LambdaTerm
+lambdaApp = [App m n | m <- lambdaTerm, n <- lambdaTerm]
+
+--lambdaLam :: GenParser Char st LambdaTerm
+lambdaLam = [Lam v t | _ <- string "\\"
+                     , v <- variableName
+                     , _ <- string "." <|> string "->"
+                     , t <- lambdaTerm]
+
+paren = between (string "(") (string ")") lambdaTerm
+
+variableName :: GenParser Char st String
+variableName = [v:primes | v <- letter, primes <- many (char '\'')]
+--variable = [x:xs | x <- lower, xs <- many alphaNum]
