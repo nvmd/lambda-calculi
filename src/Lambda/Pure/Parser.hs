9b10b4949391c5d64709e5813ec585ca8e3bda0d
Sergey Kazenyuk <kazenyuk@gmail.com>
2012-08-23 03:06:25 +0200

    Unit testing experiments in the scope of Lambda-Term parsing

--- src/Lambda/Pure/Parser.hs
+++ src/Lambda/Pure/Parser.hs
@@ -4,8 +4,15 @@ module Lambda.Pure.Parser where
 import Text.ParserCombinators.Parsec
 import Lambda.Pure.Term
 
---parseTerm :: String -> Either ParseError LambdaTerm
---parseTerm _ = fail "Not implemented"
+import Test.HUnit
+import Text.ParserCombinators.Parsec.Error(ParseError,
+											--Message, 
+											errorMessages,
+											--messageEq
+											)
+
+parseTerm :: String -> Either ParseError LambdaTerm
+parseTerm = runParser lambdaTerm () ""
 
 -- T: V | A | L | P
 -- V: <latin letter + '\''>
@@ -14,7 +21,7 @@ import Lambda.Pure.Term
 -- P: '('T')'
 
 --lambdaTerm :: GenParser Char st LambdaTerm
-lambdaTerm = lambdaVar <|> lambdaApp <|> lambdaLam <|> paren
+lambdaTerm = lambdaVar <|> lambdaLam <|> paren <|> lambdaApp
 
 --lambdaVar :: GenParser Char st LambdaTerm
 lambdaVar = [Var v | v <- variableName]
@@ -34,3 +41,45 @@ variableName :: GenParser Char st String
 variableName = [v:primes | v      <- letter
 						 , primes <- many (char '\'')]
 --variable = [x:xs | x <- lower, xs <- many alphaNum]
+
+
+lambdaVarSimple = TestCase (assertEqual "variable"
+					(Right $ Var "x")
+					(runParser lambdaVar () "" "x"))
+lambdaVarSimplePrime = TestCase (assertEqual "variable"
+						(Right $ Var "x'")
+						(runParser lambdaVar () "" "x'"))
+lambdaVarSimplePrime2 = TestCase (assertEqual "variable"
+						(Right $ Var "x''")
+						(runParser lambdaVar () "" "x''"))
+
+
+lambdaLamSimpleBound = TestCase (assertEqual "lambda-abstraction"
+						(Right $ Lam "x" (Var "x"))
+						(runParser lambdaLam () "" "\\x.x"))
+
+lambdaLamSimpleUnbound = TestCase (assertEqual "lambda-abstraction"
+						(Right $ Lam "x" (Var "y"))
+						(runParser lambdaLam () "" "\\x.y"))
+
+stubTest = TestCase (assertEqual "Unimplemented" True False)
+
+lambdaVarTests = TestList [lambdaVarSimple,
+							lambdaVarSimplePrime,
+							lambdaVarSimplePrime2]
+lambdaAppTests = TestList [TestLabel "" stubTest]
+lambdaLamTests = TestList [TestLabel "" lambdaLamSimpleBound,
+						   TestLabel "" lambdaLamSimpleUnbound]
+lambdaTermTests = TestList [TestLabel "" stubTest]
+
+tests = TestList [TestLabel "lambdaVar" lambdaVarTests,
+				  TestLabel "lambdaApp" lambdaAppTests,
+				  TestLabel "lambdaLam" lambdaLamTests,
+				  TestLabel "lambdaTerm" lambdaTermTests]
+runTests = runTestTT tests
+
+instance Eq ParseError where
+   a == b = errorMessages a == errorMessages b
+
+--instance Eq Message where
+--   (==) = messageEq
