cf0d1879bb156bcba1e88e57b7458f86e1426607
Sergey Kazenyuk <kazenyuk@gmail.com>
2013-05-02 19:05:05 +0400

    Type lambda-abstractions and products.

--- src/Lambda/P/Term.hs
+++ src/Lambda/P/Term.hs
@@ -12,8 +12,30 @@ data LambdaPTerm = Var Sym
                  | Prod Sym LambdaPTerm LambdaPTerm
                  deriving Show
 
+isTypeOrKind (Just Type) = True
+isTypeOrKind (Just Kind) = True
+isTypeOrKind _           = False
+
+isType (Just Type) = True
+isType _           = False
+
+isTypeOrKind2 j@(Just Type) = j
+isTypeOrKind2 j@(Just Kind) = j
+isTypeOrKind2 _             = Nothing
+
+isType2 j@(Just Type) = j
+isType2 _             = Nothing
+
 data Context a = Context [(Sym, a)]
                deriving Show
+
+-- empty context
+emptyCtx :: LambdaCalculus a => Context a
+emptyCtx = Context []
+
+-- extend the context
+extendCtx :: LambdaCalculus a => Sym -> a -> Context a -> Context a
+extendCtx s t (Context e) = Context $ (s, t) : e
 
 class LambdaCalculus a where
   beta :: a -> a
@@ -25,19 +47,20 @@ instance LambdaCalculus LambdaPTerm where
 
   -- unlike `classic` definition of a context, our context is right-to-left ordered
   checkCtx (Context [])         = True
-  checkCtx (Context ((x,t):xs)) = typeOrKind (doType t (Context xs)) == True
-                                where typeOrKind (Just Type) = True
-                                      typeOrKind (Just Kind) = True
-                                      typeOrKind _           = False
+  checkCtx (Context ((x,t):xs)) = isTypeOrKind (doType t (Context xs)) == True
 
   doType (Var x) g@(Context e) | (checkCtx g) = liftM snd (find (\(s, _) -> s == x) e)
                                | otherwise    = Nothing
   doType Type g | (checkCtx g) = Just Kind
                 | otherwise    = Nothing
   doType (App m n)    g = Nothing
-  doType (Lam x a m)  g = Nothing
-  doType (Prod x a b) g = Nothing
+  doType (Lam x a m)  g = isType2 $ doType a g >>  doType m (extendCtx x a g)
+                                               >>= return . (\b -> Prod x a b)
+  doType (Prod x a b) g = isType2 $ doType a g >> isTypeOrKind2 bType
+                        where bType = doType b (extendCtx x a g)
   doType _            _ = Nothing
+
+
 
 tx = (Var "x")
 tg = (Context [("x", (Var "Int")), ("Int", Type)])
